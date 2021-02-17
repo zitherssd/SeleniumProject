99ff2f09f9ab85f173110b07b524f89dec34599f
Filip Andrei <68892402+zitherssd@users.noreply.github.com>
2021-02-17 20:00:48 +0200

    Add CFR test

---
+++ SeleniumTest/SeleniumTest/Base/BaseTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumTest
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver("D:\\chrome");
+            driver.Manage().Window.Maximize();
+        }
+
+
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumTest/SeleniumTest/CfrTests/CfrRouteTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTest
+{
+    public class CfrRouteTest : BaseTest
+    {
+        private string departure = "Sinaia";
+        private string destination = "Deva";
+
+        [Test]
+        public void RouteTest()
+        {
+
+            CfrHomePage cfrHomePage = new CfrHomePage(driver);
+            cfrHomePage.GoTo();
+            cfrHomePage.ClosePopup();
+            cfrHomePage.SearchRoutes(departure,destination);
+
+            SearchResultPage searchPage = new SearchResultPage(driver);
+
+            Assert.IsTrue(searchPage.CheckTitle(departure,destination));
+        }
+    }
+}
